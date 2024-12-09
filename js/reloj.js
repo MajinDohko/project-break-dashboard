5082f715cf213cba708ef541120a710f04dbdba8
MajinDohko <agarridoluengo@gmail.com>
2024-12-09 21:14:09 +0100

    cambios js reloj

--- js/reloj.js
+++ js/reloj.js
@@ -25,21 +25,21 @@ function actualizarReloj(){
     
     //Creamos la variable frases y las condiciones para las que se imprimirá cada una
     let frases = "";
-    if (horas <= 7 && minutos >= 0) {
+    if (horas < 7 || (horas === 7 && minutos === 0)) {
         frases = "¡Es hora de dormir, deja de teclear y acuéstate!";
-    } else if ((horas >= 7 && minutos > 0) && (horas <= 9 && minutos >= 0)) {
+    } else if ((horas === 7 && minutos > 0) || (horas < 9 && minutos === 0)) {
         frases = "Prepárate, saca a la perra y tómate un café";
-    } else if ((horas >= 9 && minutos > 0) && (horas <= 12 && minutos >= 0)) {
+    } else if ((horas === 9 && minutos > 0) || (horas < 12 && minutos === 0)) {
         frases = "Es hora de hacer código";
-    } else if ((horas >= 12 && minutos > 0) && (horas <= 14 && minutos >= 0)) {
+    } else if ((horas === 12 && minutos > 0) || (horas < 14 && minutos === 0)) {
         frases = "Queda poco para el primer descanso, ¡ánimo!";
-    } else if ((horas >= 14 && minutos > 0) && (horas <= 16 && minutos >= 0)) {
+    } else if ((horas === 14 && minutos > 0) || (horas < 16 && minutos === 0)) {
         frases = "Descansa, prepara algo de comer y coge energía";
-    } else if ((horas >= 16 && minutos > 0) && (horas <= 18 && minutos >= 0)) {
+    } else if ((horas === 16 && minutos > 0) || (horas < 18 && minutos === 0)) {
         frases = "Buenas tardes, es hora del último empujón";
-    } else if ((horas >= 18 && minutos > 0) && (horas <= 21 && minutos >= 0)) {
+    } else if ((horas === 18 && minutos > 0) || (horas < 21 && minutos === 0)) {
         frases = "Ya has hecho mucho por hoy, descansa y aprovecha la tarde.";
-    } else if ((horas >= 21 && minutos > 0) && (horas <= 23 && minutos >= 0)) {
+    } else if ((horas === 21 && minutos > 0) || (horas < 23 && minutos === 0)) {
         frases = "No olvides cenar antes de irte a dormir.";
     } else {
         frases = "Es hora de irse a dormir, apaga todo y descansa";

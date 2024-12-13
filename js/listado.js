1cc690addc23b8391da3d0f515b1933e79c94ce7
MajinDohko <agarridoluengo@gmail.com>
2024-12-13 21:09:02 +0100

    corregido script del listado para que funcionen los enlaces

--- js/listado.js
+++ js/listado.js
@@ -1,12 +1,3 @@
-//Función que me trae una imagen random cada 15 segundos de la carpeta imágenes
-document.body.style.backgroundImage = "url(../img/imagen-1.jpg)";
-setInterval(imagenRandom, 15000);
-function imagenRandom(){
-    const imagenes = ["url(../img/imagen-1.jpg)", "url(../img/imagen-2.jpg)", "url(../img/imagen-3.jpg)", "url(../img/imagen-4.jpg)"];
-    const imagenAleatoria = Math.floor((Math.random()*imagenes.length));
-    document.body.style.backgroundImage = imagenes[imagenAleatoria];
-}
-
 //Traigo del DOM lo que voy a manipular
 const botonEnlace = document.getElementById('boton-enlace');
 let nombreEnlace = document.getElementById('nombre-enlace');
